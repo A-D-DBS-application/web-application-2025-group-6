2516eb5332fabcce8b06ebc2862f3f37bed33080
ArmandH <Armand.Heeren@ugent.be>
2025-12-03 13:37:52 +0100

    Update config.py

--- travel_mvp/config.py
+++ travel_mvp/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "change_me"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///travel_mvp.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:wyBuDBeVOy2kW8nV@aws-1-eu-central-1.pooler.supabase.com:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
