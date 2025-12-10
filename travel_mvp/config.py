35bc2d40bf780b9fd688a76a360be0ec61e2ecd6
ArmandH <Armand.Heeren@ugent.be>
2025-12-10 14:13:28 +0100

    update config

--- travel_mvp/config.py
+++ travel_mvp/config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "change_me"
 
     # Vul hier JE ECHTE DATABASE PASSWORD in tussen de dubbele quotes
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:30wFSILqx3dVYNgX@db.dfpsbewjkvhrietohwes.supabase.co:5432/postgres?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.dfpsbewjkvhrietohwes:30wFSILqx3dVYNgX@aws-1-eu-central-1.pooler.supabase.com:6543/postgres"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
